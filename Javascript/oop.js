6555d259f06fdc8627cc46260725b975c9ba2295
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-12 17:08:06 +0700

    update Javascript/oop.js

--- Javascript/oop.js
+++ Javascript/oop.js
@@ -5,20 +5,17 @@ function say(nama){
 say("Sean");
 
 console.info("Function Constructor")
-function Person(nama,umur,clan){
+function Person(nama,umur){
   this.nama = nama;
   this.umur = umur;
-  this.clan = function(a){
-    console.log(`${this.nama} dari clan ${a}`)
-  };
   this.asal = "";
   this.hallo = function(he){
     console.log(`Hallo ${he}, saya ${this.nama}`)
   };
 };
-const Owner = new Person("Sean","18","01");
+const Owner = new Person("Sean","18");
 Owner.asal = "Jakarta";
-const Member = new Person("Isagi","20","03");
+const Member = new Person("Isagi",);
 Member.hallo("Joko");
 console.log(Owner)
 console.log(Member)
