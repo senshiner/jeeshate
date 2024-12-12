4ae559272553e39298a0dcbdd5694708af6fff80
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-12 17:49:43 +0700

    update Javascript/oop.js

--- Javascript/oop.js
+++ Javascript/oop.js
@@ -6,29 +6,34 @@ say("Sean");
 
 console.info("Function Constructor")
 function Person(nama,umur = null){
-  this.nama = nama;
-  this.umur = umur;
-  this.asal = "";
-  this.hallo = function(he){
+  this.nama = nama; 
+  this.umur = umur; // 1
+  this.asal = ""; // 2
+  this.hallo = function(he){ // 3
     console.log(`Hallo ${he}, saya ${this.nama}`)
   };
 };
 const Owner = new Person("Sean","18");
 Owner.asal = "Jakarta";
-const Member = new Person("Isagi");
+const Member = new Person("Isagi");// umur null
 Member.hallo("Joko");
 console.log(Owner)
 console.log(Member)
 
 console.info("Function class")
 class Mobil{
-  merek = "";
+  merek = ""; // 1
   constructor(warna){
-    this.tahun = "";
-    this.warna = warna;
+    this.tahun = ""; // 2
+    this.warna = warna; // 3
+    // jangan gunakan function disini!!
+  }
+  kata(){
+    console.info(`Ini mobil baruku ${merek}`)
   }
 }
 const avanza = new Mobil("hitam")
 avanza.merek = "avanza";
 avanza.tahun = "1990";
+avanza.kata();
 console.log(avanza)
