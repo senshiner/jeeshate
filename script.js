696c4de85c0db8af8d035bab9b0e18a475a88b97
Sendi Winoto <157441609+senshiner@users.noreply.github.com>
2024-12-24 14:57:39 +0700

    update script.js

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ document.getElementById("currentDay").innerText = `Jadwal Hari Ini: ${currentDay
 let schedule = {
   "Senin": {
     time1: "06:00 - 06:30",
-    meal1: "Makan Pagi: Nasi seporsi dan 2 butir telur",
-    time2: "12:00 - 13:00",
-    meal2: "Makan Siang",
-    time3: "16:00 - 17:00",
-    meal3: "Camilan: Regal 1-2 keping dan roti 1/2 potong",
-    time4: "19:00 - 20:00",
-    meal4: "Makan Malam"
+    meal1: "Makan Pagi: Nasi seporsi dengan 2 butir telur",
+    time2: "07:30 - 08:00",
+    meal2: "Olahraga Tangan & Punggung",
+    time3: "12:00 - 13:00",
+    meal3: "Makan Siang",
+    time4: "16:00 - 17:00",
+    meal4: "Camilan: Regal 1-2 keping dan roti 1/2 potong"
   },
   "Selasa": {
     time1: "06:00 - 06:30",
     meal1: "Makan Pagi: Nasi seporsi dengan 2 butir telur",
-    time2: "12:00 - 13:00",
-    meal2: "Makan Siang",
-    time3: "16:00 - 17:00",
-    meal3: "Camilan: Regal 1-2 keping dan roti 1/2 potong",
-    time4: "19:00 - 20:00",
-    meal4: "Makan Malam"
+    time2: "07:30 - 08:00",
+    meal2: "Olahraga Kaki & Paha",
+    time3: "12:00 - 13:00",
+    meal3: "Makan Siang",
+    time4: "16:00 - 17:00",
+    meal4: "Camilan: Regal 1-2 keping dan roti 1/2 potong"
   },
   "Rabu": {
     time1: "06:00 - 06:30",
@@ -44,22 +44,22 @@ let schedule = {
   "Kamis": {
     time1: "06:00 - 06:30",
     meal1: "Makan Pagi: Nasi seporsi dengan 2 butir telur",
-    time2: "12:00 - 13:00",
-    meal2: "Makan Siang",
-    time3: "16:00 - 17:00",
-    meal3: "Camilan: Regal 1-2 keping dan roti 1/2 potong",
-    time4: "19:00 - 20:00",
-    meal4: "Makan Malam"
+    time2: "07:30 - 08:00",
+    meal2: "Olahraga Tangan & Punggung",
+    time3: "12:00 - 13:00",
+    meal3: "Makan Siang",
+    time4: "16:00 - 17:00",
+    meal4: "Camilan: Regal 1-2 keping dan roti 1/2 potong"
   },
   "Jumat": {
     time1: "06:00 - 06:30",
     meal1: "Makan Pagi: Nasi seporsi dengan 2 butir telur",
-    time2: "12:00 - 13:00",
-    meal2: "Makan Siang",
-    time3: "16:00 - 17:00",
-    meal3: "Camilan: Regal 1-2 keping dan roti 1/2 potong",
-    time4: "19:00 - 20:00",
-    meal4: "Makan Malam"
+    time2: "07:30 - 08:00",
+    meal2: "Olahraga Kaki & Paha",
+    time3: "12:00 - 13:00",
+    meal3: "Makan Siang",
+    time4: "16:00 - 17:00",
+    meal4: "Camilan: Regal 1-2 keping dan roti 1/2 potong"
   },
   "Sabtu": {
     time1: "06:00 - 06:30",
@@ -95,12 +95,3 @@ document.getElementById("meal3").innerText = schedule[currentDay].meal3;
 
 document.getElementById("time4").innerText = schedule[currentDay].time4;
 document.getElementById("meal4").innerText = schedule[currentDay].meal4;
-
-// Menambahkan pesan untuk olahraga
-if (currentDay === "Senin" || currentDay === "Kamis") {
-  document.getElementById("meal2").innerText += " - Olahraga Tangan & Punggung";
-} else if (currentDay === "Selasa" || currentDay === "Jumat") {
-  document.getElementById("meal2").innerText += " - Olahraga Kaki & Paha";
-} else if (currentDay === "Sabtu") {
-  document.getElementById("meal2").innerText += " - Olahraga Perut";
-}
